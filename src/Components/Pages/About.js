37869ea00e44822445e49f573352bc9556fe47de
Kavic07 <kavicblessing@gmail.com>
2024-02-27 15:20:40 +0100

    This is my first commit

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="about-section">
+        <h1>Everything you need to know about Whymeffs Anime Community.</h1>
+        <p className="para">Welcome, weebs to <span>Whymeffs Anime Community</span> where we give the latest news, gists and all updates about animes. Anime, a unique form of animated storytelling, has captivated hearts around the world with its vibrant characters, rich narratives, and imaginative worlds. Originating in Japan, anime has evolved into a global phenomenon, transcending cultural boundaries and leaving an indelible mark on entertainment. At its core, anime is more than just animated cartoons; it's an art form that delves into diverse genres, offering something for everyone. From epic adventures and heartwarming romances to mind-bending mysteries and futuristic sci-fi, anime explores a vast spectrum of themes and emotions.</p>
+        <p>Anime's influence extends beyond the screen, sparking creativity and inspiring a global subculture. Enthusiastic communities gather to discuss their favorite series, create fan art, and attend conventions celebrating the diverse world of anime. The art style itself is iconic, with characters often sporting exaggerated features that convey a wide range of emotions. One of the distinctive features of anime is its ability to convey profound messages and explore complex human experiences. Characters grapple with identity, morality, and the essence of existence, creating narratives that resonate deeply with viewers. This emotional depth, combined with stunning visuals and intricate storytelling, sets anime apart as a powerful and evocative medium</p>
+        <p>From classics like <span>Naruto</span> and <span>Dragon Ball</span> to modern masterpieces like <span>Attack on Titan</span> and <span>My Hero Academia</span>, anime continues to evolve, pushing creative boundaries and challenging conventional narratives. Streaming platforms have made anime accessible to a global audience, allowing fans to explore the vast and ever-growing library of animated wonders. Whether you're a seasoned anime enthusiast or a newcomer curious to explore this captivating realm, anime invites you to embark on an extraordinary journey filled with excitement, emotion, and endless possibilities. As the animated tales unfold, they invite you to ponder life's complexities, celebrate diversity, and embrace the magic that is uniquely anime.</p>
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+  return (
+    <div className="home-wrapper">
+        <h2>Welcome to <span>Whymeffs</span> <span>Anime</span> <span>Community</span></h2>
+        <p>Beyond the <span>screen lies a tapestry of emotions waiting to</span> be unraveled.<br></br> Anime is not just <span>entertainment, but an odyssey through the heart and soul</span> of storytelling.<br></br>Discover a symphony of <span>colors, emotions, and uncharted worlds. Anime, because the universe is too</span> small for just one reality.</p>
+        <button><Link className="btn-link" to="https://animepahe.ru/" >Explore Your Anime World</Link></button>
+    </div>
+  )
+}
+
+export default Home
