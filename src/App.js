3284dc7b63893bfd7c0d36cf50343e6b7cb93139
Kavic07 <kavicblessing@gmail.com>
2024-04-09 12:23:10 +0100

    few changes in the navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import Home from './Components/Pages/Home';
-import About from './Components/Pages/About';
-import Contact from './Components/Pages/Contact';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Contact from "./Components/Pages/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-      <>
-        <BrowserRouter>
-          <Navbar/>
-          <Routes>
-              <Route path="/" element={<Home/>}></Route>
-              <Route path="/about" element={<About/>}></Route>
-              <Route path="/contact" element={<Contact/>}></Route>
-          </Routes>
-        </BrowserRouter>
-      </>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,34 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => {
   return (
-    
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="#">LOGO</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/about">About</Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="dd">
+            LOGO
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/about">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-    
   );
 };
 
